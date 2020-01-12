f20144611353ac728eb9132886ec8a6ef54c904c
Miles Budden <miles@pop-os.localdomain>
2020-01-12 01:49:46 +0000

    Added a pretty theme

--- src/references/static/tailwind.config.js
+++ src/references/static/tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: '#232946',
+        headline: '#fffffe',
+        paragraph: '#b8c1ec',
+        button: '#eebbc3',
+        buttontext: '#232946',
+        stroke: '#121629'
+      },  
+    }
   },
   variants: {},
   plugins: []
