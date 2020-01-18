e6739c4a2792b8f9f7c326ee4628ca27a5a83f02
Miles Budden <miles@budden.net>
2020-01-18 16:08:29 +0000

    Added start to 404 page

--- src/references/static/tailwind.config.js
+++ src/references/static/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // background: '#fec7d7',
+        // headline: '#0e172c',
+        // paragraph: '#0e172c',
+        // button: '#0e172c',
+        // buttontext: '#fffffe',
+        // stroke: '#0e172c'
         background: '#232946',
         headline: '#fffffe',
         paragraph: '#b8c1ec',
--- src/references/views.py
+++ src/references/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'references/signin.html')
+    return render(request, 'references/404.html')
