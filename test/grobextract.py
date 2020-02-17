bc0aaab409a548c49b66a05a210ac94923392139
Miles Budden <miles@budden.net>
2020-02-17 20:11:40 +0000

    Updated the pdf extractor

--- test/grobextract.py
+++ test/grobextract.py
@@ -1,4 +1,29 @@
 import requests
-files = {'input': (open('better.pdf', 'rb'))}
+from bs4 import BeautifulSoup
+import sys
+
+
+files = {'input': (open(sys.argv[1], 'rb'))}
 r = requests.post("http://localhost:8070/api/processHeaderDocument", files=files)
-print(r.text)
+
+try:
+    soup = BeautifulSoup(r.text, "xml")
+    print("Title:", soup.find("title").string)
+except AttributeError:
+    print("Title not found")
+
+a = []
+try:
+    authors = soup.find_all("author")
+    for author in authors:
+        try:
+            a.append(author.persName.forename.string + " " + author.persName.surname.string)
+        except AttributeError:
+            # print("Not valid author")
+            ...
+except AttributeError:
+    print("No authors found")
+
+print("\n".join(list(set(a))))
+# print(len(list(set(a))))
+# print(r.text)
