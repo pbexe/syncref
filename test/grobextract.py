abca0eef24e238db08dd757fed965e07d8be3fbc
Miles Budden <miles@pop-os.localdomain>
2020-02-17 21:27:00 +0000

    BibTeX is not generated

--- test/grobextract.py
+++ test/grobextract.py
@@ -1,29 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 import sys
+from habanero import Crossref, cn
+
+def extract(fp):
+    files = {'input': fp}
+    r = requests.post("http://localhost:8080/api/processHeaderDocument", files=files)
+    title = ""
+    try:
+        soup = BeautifulSoup(r.text, "xml")
+        print("Title:", soup.find("title").string)
+        title = soup.find("title").string
+    except AttributeError:
+        print("Title not found")
+
+    a = []
+    try:
+        authors = soup.find_all("author")
+        for author in authors:
+            try:
+                a.append(author.persName.forename.string + " " + author.persName.surname.string)
+            except AttributeError:
+                # print("Not valid author")
+                ...
+    except AttributeError:
+        print("No authors found")
 
 
-files = {'input': (open(sys.argv[1], 'rb'))}
-r = requests.post("http://localhost:8070/api/processHeaderDocument", files=files)
+    cr = Crossref(mailto="miles@budden.net")
+    print("Query:", title + " " + " ".join(list(set(a))))
+    r = cr.works(query=title + " " + " ".join(list(set(a))))
+    if r["status"] == "ok":
+        print("Result Title:", r["message"]["items"][0]["title"][0])
+        # print("DOI:", r["message"]["items"][0]["DOI"])
+        print("\n=========================\n")
+        print(cn.content_negotiation(ids = r["message"]["items"][0]["DOI"], format = "bibentry"))
+    else:
+        ... # Uh oh
 
-try:
-    soup = BeautifulSoup(r.text, "xml")
-    print("Title:", soup.find("title").string)
-except AttributeError:
-    print("Title not found")
 
-a = []
-try:
-    authors = soup.find_all("author")
-    for author in authors:
-        try:
-            a.append(author.persName.forename.string + " " + author.persName.surname.string)
-        except AttributeError:
-            # print("Not valid author")
-            ...
-except AttributeError:
-    print("No authors found")
 
-print("\n".join(list(set(a))))
+if __name__ == "__main__":
+    with open(sys.argv[1], "rb") as fp:
+        extract(fp)
 # print(len(list(set(a))))
 # print(r.text)
