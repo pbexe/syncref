ffe9643598444f20843c57766f68334ef8909cf0
Miles Budden <miles@budden.net>
2020-02-18 14:40:47 +0000

    The extract function now returns BibTeX only

--- test/grobextract.py
+++ test/grobextract.py
@@ -7,13 +7,26 @@ from habanero import Crossref, cn
 
 
 def extract(fp):
-    files = {"input": fp,
-             "consolidateHeader": "1"}
-    r = requests.post("http://localhost:8080/api/processHeaderDocument", files=files, data={
-        "consolidateHeader": "1"
-    })
+    """Extracts the BibTex entry for a given PDF file
+    
+    Arguments:
+        fp {File} -- A hanfle to the PDF file to extract
+    
+    Raises:
+        ExtractionError: An error has occured when extracting the BibTeX entry
+    
+    Returns:
+        str -- The BibTeX entry
+    """
+    
+    # Prepare the PDF for multipart upload to the server
+    files = {"input": fp}
+    
+    # Upload the file to the server
+    r = requests.post("http://localhost:8080/api/processHeaderDocument", files=files)
     title = ""
     
+    # Try fetching the name and authors from the results
     try:
         soup = BeautifulSoup(r.text, "xml")
         # print("Title:", soup.find("title").string)
@@ -35,7 +48,7 @@ def extract(fp):
         # print("No authors found")
         ...
 
-
+    # Search for the paper on Crossref
     cr = Crossref(mailto="miles@budden.net")
     if a:
         r = cr.works(query=title + " " + a[0])
@@ -46,19 +59,22 @@ def extract(fp):
         for result in r["message"]["items"]:
             # If the titles are similar enough
             if SequenceMatcher(None, result["title"][0].upper(), title.upper()).ratio() > 0.9:
-                # print("Result Title:", result["title"][0])
-                # print("\n=========================\n")
-                # print(cn.content_negotiation(ids = result["DOI"], format = "bibentry"))
+                # If the title is similar enough, perform content negotiaiton
                 BibTeX = cn.content_negotiation(ids = result["DOI"], format = "bibentry")
                 break
         else:
             raise ExtractionError("No matches found")
-
     else:
         raise ExtractionError("Error with Crossref API")
     return BibTeX
 
+
 class ExtractionError(Exception):
+    """Thrown when there is an error extracting
+    
+    Arguments:
+        Exception {Exception} -- The base Python exception
+    """
     pass
 
 
